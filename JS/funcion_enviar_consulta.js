c3b820d7322db2ef9c32498303eb692e6fe88cd7
nicodacci <109529196+nicodacci@users.noreply.github.com>
2023-02-10 17:41:25 -0300

    Trabajo final Argentina Programa - CV - Nicolas Dacci - v10/2

---
+++ JS/funcion_enviar_consulta.js
@@ -0,0 +1,41 @@
+//-----------conexión con el html--------------------
+//asigno a una variable o constante una parte del html para
+//manipularla desde JS
+
+const btn_contacto = document.getElementById("contactame")
+const formulario = document.getElementById("form")
+const btn_enviar = document.getElementById("btn_enviar")
+
+const nombre_apellido = document.getElementById("nombre_apellido")
+const e_mail = document.getElementById("e-mail")
+const consulta = document.getElementById("consulta")
+
+//------mantengo a la escucha de un evento el boton----
+
+btn_contacto.addEventListener("click", mostrar_formulario)
+
+//-----------funciones----------------------------------
+
+function mostrar_formulario(){
+    btn_contacto.style.display = "none"
+    formulario.style.display = "block"
+    btn_enviar.addEventListener("click", enviar_consulta)
+}
+
+function enviar_consulta(){
+    if(formulario_completo()){
+        alert("Su consulta fue enviada")
+        btn_contacto.style.display = "block"
+        formulario.style.display = "none"
+    } else {alert("Complete los campos requeridos..")}
+
+    nombre_apellido.value=""
+    e_mail.value=""
+    consulta.value=""
+}
+
+function formulario_completo(){
+    if(nombre_apellido.value !== "" && e_mail !== "" && consulta.value !== ""){
+        return true
+    }
+}
---
+++ JS/funcion_ver_mas.js
@@ -0,0 +1,50 @@
+const ver_mas = document.getElementsByClassName("ver_mas")
+const experiencia = document.getElementsByClassName("experiencia")
+
+//como usé className retorna una coleccion de elementos similar a un array
+//accedo a cada elemento de la clase recorriendo con un for y escuchando
+//a cada escucha le asigno una funcion, que coresponderá al mismo elemento que disparó el evento
+//y al retirar el mouse del disparador le di un delay para que retire la muestra
+
+for (let i = 0; i < 2; i++) {
+    ver_mas[i].addEventListener("mouseover", function(){experiencia[i].style.display = "block"})
+    ver_mas[i].addEventListener("mouseleave", function(){
+        setTimeout(() => {
+            experiencia[i].style.display = "none"
+        }, 5000);
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function verBoton(){
+    btn_ver.style.display = "inline-block"       
+    console.log("funciona mouse over")
+}
+
+
+
+const btn_ver = document.getElementById("btn_ver")
+btn_ver.addEventListener("click", verTexto)
+
+const experiencia_laboral = document.getElementById("experiencia_laboral")
+
+
+
+
+function verTexto(){
+
+    console.log("funciona click boton")
+}
+*/
